7ba4df520e969e29cded38af49395c775dcc668c
Chandra Sekhar Vipparla <code.chandrasekhar@gmail.com>
2025-11-07 09:48:57 +0530

    feat: implement task1 – project setup and base structure

---
+++ src/main/java/org/chandra/smartparkingslotbookingsystem/SmartParkingSlotBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.chandra.smartparkingslotbookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingSlotBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingSlotBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/chandra/smartparkingslotbookingsystem/SmartParkingSlotBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chandra.smartparkingslotbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingSlotBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
